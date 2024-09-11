334d0c638305d57cd0c0c710b09790379dbc6b90
JulioRena <juliomoreira.rena@gmail.com>
2024-09-10 22:33:30 -0300

    fixing bug on database.py

--- database.py
+++ database.py
@@ -5,28 +5,33 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 
-
+# Carregar variáveis do arquivo .env
 load_dotenv()
 
+# Configuração do banco de dados PostgreSQL
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASSWORD")
+DB_PASS = os.getenv("DB_PASS")
 
-def salvar_no_postgres(dados:Vendas):
+# Função para salvar os dados validados no PostgreSQL
+def salvar_no_postgres(dados: Vendas):
+    """
+    Função para salvar no postgres
+    """
     try:
         conn = psycopg2.connect(
-            host = DB_HOST,
-            database = DB_NAME,
-            user = DB_USER,
-            password = DB_PASS
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS
         )
         cursor = conn.cursor()
         
+        # Inserção dos dados na tabela de vendas
         insert_query = sql.SQL(
-            "INSERT INTO vendas (email, data, valor, quantidade, produto) VALUES (%S, %S, %S, %S, %S)"
+            "INSERT INTO vendas (email, data, valor, quantidade, produto) VALUES (%s, %s, %s, %s, %s)"
         )
-        
         cursor.execute(insert_query, (
             dados.email,
             dados.data,
@@ -37,6 +42,6 @@ def salvar_no_postgres(dados:Vendas):
         conn.commit()
         cursor.close()
         conn.close()
-        st.success("Dados salvos com sucesso")
+        st.success("Dados salvos com sucesso no banco de dados!")
     except Exception as e:
-        st.error(f"Erro ao salvar no banco de dados {e}")
+        st.error(f"Erro ao salvar no banco de dados: {e}")
